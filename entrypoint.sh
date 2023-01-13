117c37c87c7778920ae161632b85b16ddf87593c
Gratien Dhaese <gratien.dhaese@gmail.com>
2023-01-13 10:16:05 +0100

    #1 fix the missing IP address after restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@
 #set -e
 HOSTNAME=$(cat /etc/hostname)
 #SERVER=$(/usr/local/bin/kubectl -n graphite describe pod graphite-0 | grep -i node: | cut  -d/ -f2)
-SERVER=$(/usr/bin/kubectl -n graphite get pods -o wide | tail -1 | awk '{print $6}')
 while true
 do
+  # move the SERVER line inside the loop as at each restart the graphite pod gets a new IP address
+  SERVER=$(/usr/bin/kubectl -n graphite get pods -o wide | tail -1 | awk '{print $6}')
   cpu_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
   #cpu_temp=$(($cpu_temp/1000))
   cpu_temp=$(expr $cpu_temp / 1000)
