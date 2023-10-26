33068ec63f71925a6f06e88958a0b14ecdcecff1
Gratien Dhaese <gratien.dhaese@gmail.com>
2023-10-26 11:37:59 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 # Build the pi4-temperature2graphite container image with a Dockerfile
 # To build with a new version: ./build.sh v1.1
 REL=${1:-v1.0}
+if [[ "$1" == "" ]]; then
+  # no argument given - we will try to see if we already have old images locally
+  CURRENT_SUB_VERSION=$(docker image ls 2>/dev/null| grep pi4-temperature | grep -v latest | head -1 | awk '{print $2}' | cut -d. -f2)
+  if [[ "$CURRENT_SUB_VERSION" == "" ]]; then
+    REL=${1:-v1.0}
+  else
+    NEW_SUB_VERSION=$((CURRENT_SUB_VERSION + 1))
+    REL="v1.${NEW_SUB_VERSION}"
+  fi
+else
+  # We have an argument on the command line, e.g. v1.6
+  REL=${1:-v1.0}
+fi
 cat ~/.ghcr-token | docker login ghcr.io -u gdha --password-stdin
 echo "Copy /usr/local/bin/k3s to $PWD"
 cp /usr/local/bin/k3s .
